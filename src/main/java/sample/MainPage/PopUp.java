5f4f96214318b5d8f2161eec4d37aa355d744073
Andra Muntean <andramuntean@Andras-MacBook-Air.local>
2021-04-17 14:39:07 +0300

    CC-66 Added required fields in the pop up

--- src/main/java/sample/MainPage/PopUp.java
+++ src/main/java/sample/MainPage/PopUp.java
@@ -1,4 +1,17 @@
 package sample.MainPage;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class PopUp {
+    @FXML
+    private ComboBox comboBox1;
+
+    @FXML
+    private void initialize(){
+        comboBox1.getItems().add("Processors");
+        comboBox1.getItems().add("RAM");
+        comboBox1.getItems().add("Graphic Cards");
+        comboBox1.getItems().add("Power Supply Unit");
+    }
 }
